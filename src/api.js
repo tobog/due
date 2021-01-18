3aa1b94179e1330e8b4cd8623cbb4b3c657fa0e7
yankui <yankui@baidu.com>
2021-01-18 20:26:39 +0800

    fix：popper

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import Components from "./components/index";
 import directives from "./directives/index";
 import utils from "./utils/index";
 
-const install = function(Vue, options = {}) {
+const install = function (Vue, options = {}) {
     if (install.installed && !options.reset) return;
     console.log(options);
     global.__VUE__ = Vue;
@@ -12,22 +12,67 @@ const install = function(Vue, options = {}) {
     const compPrefix = options.compPrefix || "";
     const compPrefixUpper = compPrefix.toUpperCase();
     const langMap = options.langMap;
-    const prototype = Vue.prototype || Vue.__proto__;
-    Object.defineProperty(prototype, "__$cssPrefix__", {
-        get() {
-            return cssPrefix;
-        },
-    });
-    Object.defineProperty(prototype, "__$langPrefix__", {
-        get() {
-            return langPrefix;
-        },
-    });
-
-    if (langMap && typeof langMap === "object") {
-        Object.defineProperty(prototype, "__$langMap__", {
+    if (Vue.prototype) {
+        Vue.prototype.__$cssPrefix__ = cssPrefix;
+        Vue.prototype.__$langPrefix__ = langPrefix;
+        if (langMap && typeof langMap === "object") {
+            Vue.prototype.__$langMap__ = langMap;
+        }
+        Vue.prototype.[`$${compPrefixUpper}VueWraper`] = Components.VueWraper;
+        Vue.prototype.[`$${compPrefixUpper}Modal`] = Components.Modal;
+        Vue.prototype.[`$${compPrefixUpper}LoadingBar`] = Components.LoadingBar;
+        Vue.prototype.[`$${compPrefixUpper}Notice`] = Components.Notice;
+        Vue.prototype.[`$${compPrefixUpper}Message`] = Components.Message;
+        Vue.prototype.[`$${compPrefixUpper}Loading`] = Components.Loading;
+    } else {
+        Object.defineProperty(Vue.__proto__, "__$cssPrefix__", {
             get() {
-                return langMap;
+                return cssPrefix;
+            },
+        });
+        Object.defineProperty(Vue.__proto__, "__$langPrefix__", {
+            get() {
+                return langPrefix;
+            },
+        });
+
+        if (langMap && typeof langMap === "object") {
+            Object.defineProperty(Vue.__proto__, "__$langMap__", {
+                get() {
+                    return langMap;
+                },
+            });
+        }
+        Object.defineProperties(Vue.__proto__, {
+            [`$${compPrefixUpper}VueWraper`]: {
+                get() {
+                    return Components.VueWraper;
+                },
+            },
+            [`$${compPrefixUpper}Modal`]: {
+                get() {
+                    return Components.Modal;
+                },
+            },
+            [`$${compPrefixUpper}LoadingBar`]: {
+                get() {
+                    return Components.LoadingBar;
+                },
+            },
+            [`$${compPrefixUpper}Notice`]: {
+                get() {
+                    return Components.Notice;
+                },
+            },
+            [`$${compPrefixUpper}Message`]: {
+                get() {
+                    return Components.Message;
+                },
+            },
+            [`$${compPrefixUpper}Loading`]: {
+                get() {
+                    return Components.Loading;
+                },
             },
         });
     }
@@ -38,41 +83,10 @@ const install = function(Vue, options = {}) {
             },
         },
     });
-    Object.keys(Components).forEach(function(key) {
+    Object.keys(Components).forEach(function (key) {
         Vue.component(`${compPrefix}${key}`, Components[key]);
     });
-    Object.defineProperties(prototype, {
-        [`$${compPrefixUpper}VueWraper`]: {
-            get() {
-                return Components.VueWraper;
-            },
-        },
-        [`$${compPrefixUpper}Modal`]: {
-            get() {
-                return Components.Modal;
-            },
-        },
-        [`$${compPrefixUpper}LoadingBar`]: {
-            get() {
-                return Components.LoadingBar;
-            },
-        },
-        [`$${compPrefixUpper}Notice`]: {
-            get() {
-                return Components.Notice;
-            },
-        },
-        [`$${compPrefixUpper}Message`]: {
-            get() {
-                return Components.Message;
-            },
-        },
-        [`$${compPrefixUpper}Loading`]: {
-            get() {
-                return Components.Loading;
-            },
-        },
-    });
+
 };
 
 if (window !== void 0) {
